f243bea63d5331656d3d964d241f09103d7d952b
Kara Taylor <tay21020@byui.edu>
2023-04-26 16:38:58 -0600

    started writing code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome.");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Enter your username: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Enter your user number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int number1)
+    {
+        int squared = number1 * number1;
+        return squared;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the number squared is {square}.");
+    }
     }
 }
