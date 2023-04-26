c9fb9237f385257d1571ab5a7a4737130c22f967
Kara Taylor <tay21020@byui.edu>
2023-04-26 16:05:15 -0600

    started writing code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,8 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
+        int number = randomGenerator.Next(1,100);
 
-        
+        int guessNumber = 0;
+        Console.WriteLine("Guess a number: ");
+        string guessString = Console.ReadLine();
+        guessNumber = int.Parse(guessString);
+
+        while (guessNumber != number)
+        {
+            if (guessNumber < number)
+            {
+                Console.WriteLine("Your guess is too small.");
+            }
+
+            if (guessNumber > number)
+            {
+                Console.WriteLine("Your guess is too big.");
+            }
+            Console.WriteLine("Guess a number: ");
+            guessString = Console.ReadLine();
+            guessNumber = int.Parse(guessString);
+        }
+
+        Console.WriteLine("That was the right number.");
     }
 }
