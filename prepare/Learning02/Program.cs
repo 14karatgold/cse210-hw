f9f73ead67c31582d94b4aeac8ec9e358e5ff684
Kara Taylor <tay21020@byui.edu>
2023-05-08 08:40:33 -0600

    finished resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -16,7 +16,17 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-    job1.Display();
-    job2.Display();
+    //job1.Display();
+    //job2.Display();
+
+    Resume myResume = new Resume();
+    myResume._name = "Allison Rose";
+
+    //myResume._jobs[0]._jobTitle;
+
+    myResume._jobs.Add(job1);
+    myResume._jobs.Add(job2);
+
+    myResume.Display();
     }
 }
