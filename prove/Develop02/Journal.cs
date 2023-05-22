173d30264a2a6d9550d2c8d3af413935b0fb7d73
Kara Taylor <tay21020@byui.edu>
2023-05-21 20:56:10 -0600

    journal class

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,6 +1,27 @@
-using System;
+using System.Collections.Generic;
 
-public class Journal
+namespace Develop02
 {
+    public class Journal
+    {
+        public List<Entry> entries;
 
-} 
+        public Journal()
+        {
+            entries = new List<Entry>();
+        }
+        
+        public List<Entry> GetAllEntries()
+        {
+            return entries;
+        }
+
+        public void StoreEntry(Entry entry)
+        {
+            if (!entries.Contains(entry))
+            {
+                entries.Add(entry);
+            }
+        }
+    }
+}
